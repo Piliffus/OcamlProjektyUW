1fda200dcdeff7f2076d70444f904cac472458d0
Filip Bienkowski <bienkowskifilip@gmail.com>
2019-11-27 01:04:05 +0100

    Switch to tuples

--- Zadanie4/iSet.ml
+++ Zadanie4/iSet.ml
@@ -25,19 +25,14 @@
 
 (* Definicje typow *)
 
-(* Typ drzewa przedzialow, oraz typ jego wezla *)
+(* 
+    Typ drzewa przedzialow, oraz typ jego wezla: 
+    lewy syn, wartosc, prawy syn, wysokosc, liczba elementow w poddrzewach.
+    Wysokosc >=1, liczba el. >= 0, wartosc to zakres (int, int).
+*)
 type t =
-  | Empty
-  | Node of node
-
-and node = 
-{
-    left : t;
-    right : t;
-    height : int;
-    value : (int * int);
-    elementy_poddrzew : int
-}
+    | Node of t * (int * int) * t * int * int
+    | Empty
 
 (* Wyjatki *)
 
@@ -54,20 +49,26 @@ exception Bledny_Przedzial
     zaczyna sie od wiekszych wartosci, dodatnia oznacza ze przedzial 'a'. 
     0 oznacza ze przedzialy sie przecinaja.
 *)
-let cmp (a1, a2) (b1, b2) =
-    if a2 < b1 then -1
-    else if b2 < a1 then 1
-    else 0
-(* Funkcja odczytująca wysokosc z zadanego wezla 'x' *)
+let cmp (a1, a2) (b1, b2) = 
+    if a2 < b1 then -1 else
+    if b2 < a1 then 1 else 0
+
+(* Funkcja odczytująca wysokosc z zadanego wezla *)
 let wysokosc =
-    function
-    | Node({height = h}) -> h 
+  function
+    | Node(_, _, _, h, _) -> h
     | Empty -> 0
 
 (* zwraca liczbę elementów w obu poddrzewach *)
-let elementy_poddrzewa =
+let elementy_poddrzew =
     function
-    | Node ({elementy_poddrzew = e}) -> e
+    | Node(_, _, _, _, e) -> e
+    | Empty -> 0
+
+(* Wielkosc przedziału w wezle *)
+let rozmiar_przedzialu =
+    function
+    | Node(_, (v1, v2), _, _, _) -> abs (v2 - v1) + 1
     | Empty -> 0
 
 (* Zwraca sumę liczb lub max_int jeśli przekracza ona max_int *)
@@ -76,18 +77,10 @@ let suma a b c d =
     then max_int
     else a + b + c + d
 
-(* Wielkosc przedziału w wezle *)
-let rozmiar_przedzialu =
-    function
-    | Node ({value = (v1, v2)}) -> abs (v2 - v1) + 1
-    | Empty -> 0
-
 (* Konstruktor wezla o poddrzewach 'l', 'r' i przedziale 'v' *)
 let make l v r =
-    Node({left = l; value = v; right = r; 
-          height = max (wysokosc l) ((wysokosc r) + 1); 
-          elementy_poddrzew = suma 
-          (elementy_poddrzewa l) (rozmiar_przedzialu l) (elementy_poddrzewa r) (rozmiar_przedzialu r)})
+    Node(l, v, r, max (wysokosc l) (wysokosc r) + 1, 
+    suma (elementy_poddrzew l) (rozmiar_przedzialu l) (elementy_poddrzew r) (rozmiar_przedzialu r))
 
 (* 
     Balansuje drzewo utworzone z poddrzew 'l' i 'r' oraz zakresu 'v'
@@ -99,74 +92,25 @@ let bal l v r =
     let hr = wysokosc r in
     if hl > hr + 2 then
         match l with
-        | Node({left = ll; value = lv; right = lr;}) ->
-                if wysokosc ll >= wysokosc lr then make ll lv (make lr v r)
-                else (match lr with
-                | Node({left = lrl; value = lrv; right = lrr;}) ->
-                        make (make ll lv lrl) lrv (make lrr v r)
-                | Empty -> assert false)
+        | Node(ll, lv, lr, _, _) ->
+            if wysokosc ll >= wysokosc lr then make ll lv (make lr v r)
+            else
+            (match lr with
+            | Node(lrl, lrv, lrr, _, _) ->
+                make (make ll lv lrl) lrv (make lrr v r)
+            | Empty -> assert false)
         | Empty -> assert false
     else if hr > hl + 2 then
         match r with
-        | Node({left = rl; value = rv; right = rr;}) ->
-                if wysokosc rr >= wysokosc rl then make (make l v rl) rv rr
-                else (match rl with
-                | Node({left = rll; value = rlv; right = rlr;}) ->
-                        make (make l v rll) rlv (make rlr rv rr)
-                | Empty -> assert false)
+        | Node(rl, rv, rr, _, _) ->
+            if wysokosc rr >= wysokosc rl then make (make l v rl) rv rr
+            else
+            (match rl with
+            | Node(rll, rlv, rlr, _, _) ->
+                make (make l v rll) rlv (make rlr rv rr)
+            | Empty -> assert false)
         | Empty -> assert false
     else make l v r
-
-(* dodaje przedział 'x' do drzewa 's' zakładając, że jest rozłączny z wszystkimi przedziałami *)
-let rec add_one x =
-    function
-    | Node({left = l; value = v; right = r; height = h}) ->
-            let c = cmp x v in
-            if c = 0 then make l x r 
-            else if c < 0 
-                then
-                let nl = add_one x l
-                in bal nl v r
-                else
-                let nr = add_one x r
-                in bal l v nr
-    | Empty -> make Empty x Empty
-
-(* Zwraca drzewo zawierające elementy z 'l' 'r' i wezel z przedzialem 'v' *)
-let rec join l v r =
-    match (l, r) with
-    | (Empty, _) -> add_one v r
-    | (_, Empty) -> add_one v l
-    | Node({left = ll; value = lv; right = lr; height = lh}), 
-      Node({left = rl; value = rv; right = rr; height = rh}) ->
-            if lh > rh + 2 then bal ll lv (join lr v r) else
-            if rh > lh + 2 then bal (join l v rl) rv rr else 
-            make l v r
-
-(* 
-    Zwraca drzewo powstałe po złączeniu 's1' z 's2' w taki sposób, że
-    w węźle nowo powstałego drzewa jest minimalny element  drzewa 's2'
-*)
-let rec sklej s1 s2 =
-    let rec min_element =
-        function
-        | Node({left = Empty; value = v}) -> v
-        | Node({left = l}) -> min_element l
-        | Empty -> raise Not_found
-    in
-    let rec usun_min_element =
-        function
-        | Node({left = Empty; right = r}) -> r
-        | Node({left = l; value = v; right = r}) -> bal (usun_min_element l) v r
-        | Empty -> invalid_arg "blad"
-    in
-    match s1, s2 with
-    | Empty, _ -> s2
-    | _, Empty -> s1
-    | _ -> 
-    let r = min_element s2 in
-    let s3 = usun_min_element s2 in
-    join s1 r s3
 
 (* 
     Znajduje w 's' skrajnie lewy i prawy przedział dla
@@ -176,38 +120,61 @@ let rec sklej s1 s2 =
     rzuca wyjatek.
 *)
 let znajdz_skrajne s v =
-    let rec znajdz_lewy_skrajny s (q1, q2) = 
-    match s with
-    | Empty -> raise Brak_Skrajnego_Przedzialu
-    | Node({left = l; value = (v1, v2); right = r}) ->
-        if (v1 <= q1 && q1 <= v2) || (v2 = q1 - 1 && q1 != min_int) then (v1, v2) 
-        else if v1 > q1 then
-            try znajdz_lewy_skrajny l (q1, q2) with
-                | Brak_Skrajnego_Przedzialu ->
-                    if v1 <= q2 then (v1, v2)
-                    else raise Brak_Skrajnego_Przedzialu
-        else znajdz_lewy_skrajny r (q1, q2)
+    let rec znajdz_lewy_skrajny s (a1, a2) = 
+        match s with
+        | Empty -> raise Brak_Skrajnego_Przedzialu
+        | Node(l, (v1, v2), r, _, _) ->
+        if (v1 <= a1 && a1 <= v2) || (v2 = a1 - 1 && a1 != min_int) then (v1, v2) else
+            if v1 > a1 then
+            try znajdz_lewy_skrajny l (a1, a2) with
+            | Brak_Skrajnego_Przedzialu ->
+                if v1 <= a2 then (v1, v2)
+                else raise Brak_Skrajnego_Przedzialu
+            else znajdz_lewy_skrajny r (a1, a2) 
     in
-    let rec znajdz_prawy_skrajny s (q1, q2) = 
-    match s with
-    | Empty -> raise Brak_Skrajnego_Przedzialu
-    | Node({left = l; value = (v1, v2); right = r}) ->
-        if (v1 <= q2 && q2 <= v2) || (v1 = q2 + 1 && q2 != max_int) then (v1, v2) 
-        else if v2 < q2 then
-            try znajdz_prawy_skrajny r (q1, q2) with
-                | Brak_Skrajnego_Przedzialu ->
-                    if q1 <= v2 then (v1, v2)
-                    else raise Brak_Skrajnego_Przedzialu
-        else znajdz_prawy_skrajny l (q1, q2) 
+    let rec znajdz_prawy_skrajny s (a1, a2) = 
+        match s with
+        | Empty -> raise Brak_Skrajnego_Przedzialu
+        | Node(l, (v1, v2), r, _, _) ->
+        if (v1 <= a2 && a2 <= v2) || (v1 = a2 + 1 && a2 != max_int) then (v1, v2) else
+            if v2 < a2 then
+            try znajdz_prawy_skrajny r (a1, a2) with
+            | Brak_Skrajnego_Przedzialu ->
+                if a1 <= v2 then (v1, v2)
+                else raise Brak_Skrajnego_Przedzialu
+            else znajdz_prawy_skrajny l (a1, a2) 
     in
-    let v1 =
-        try znajdz_lewy_skrajny s v with
-        | Brak_Skrajnego_Przedzialu -> v
-    and v2 =
-        try znajdz_prawy_skrajny s v with
-        | Brak_Skrajnego_Przedzialu -> v
+    let lewy_skrajny =
+    try znajdz_lewy_skrajny s v with
+    | Brak_Skrajnego_Przedzialu -> v
+    in
+    let prawy_skrajny =
+    try znajdz_prawy_skrajny s v with
+    | Brak_Skrajnego_Przedzialu -> v
     in 
-    (v1, v2)
+    (lewy_skrajny, prawy_skrajny)
+
+(* dodaje przedział 'x' do drzewa 's' zakładając, że jest rozłączny z wszystkimi przedziałami *)
+let rec add_one x =
+    function
+    | Node(l, v, r, h, _) ->
+        let c = cmp x v in
+        if c = 0 then make l x r 
+        else if c < 0 then
+            bal (add_one x l) v r
+        else
+            bal l v (add_one x r)
+    | Empty -> make Empty x Empty
+
+(* Zwraca drzewo zawierające elementy z 'l' 'r' i wezel z przedzialem 'v' *)
+let rec join l v r =
+    match (l, r) with
+    | (Empty, _) -> add_one v r
+    | (_, Empty) -> add_one v l
+    | (Node(ll, lv, lr, lh, _), Node(rl, rv, rr, rh, _)) ->
+        if lh > rh + 2 then bal ll lv (join lr v r) else
+            if rh > lh + 2 then bal (join l v rl) rv rr else 
+                make l v r
 
 (* 
     Zwraca krotke (l, present, r), gdzie 'l' to drzewo zawierające 
@@ -218,7 +185,7 @@ let rozdziel x s =
     let rec walk x =
         function
         | Empty -> (Empty, false, Empty)
-        | Node({left = l; value = v; right = r;}) ->
+        | Node(l, v, r, _, _) ->
             let c = cmp x v in
             if c = 0 then (l, true, r) else
                 if c < 0 then
@@ -227,17 +194,42 @@ let rozdziel x s =
                 else
                 let (lr, pres, rr) = walk x r
                 in (join l v lr, pres, rr)
-    in 
-    walk x s
+  in 
+  walk x s
+
+(* 
+    Zwraca drzewo powstałe po złączeniu 's1' z 's2' w taki sposób, że
+    w węźle nowo powstałego drzewa jest minimalny element  drzewa 's2'
+*)
+let rec sklej s1 s2 = 
+    let rec min_element =
+        function
+        | Node(Empty, v, _, _, _) -> v
+        | Node(l, _, _, _, _) -> min_element l
+        | Empty -> raise Not_found
+    in
+    let rec usun_min_element =
+        function
+        | Node(Empty, _, r, _, _) -> r
+        | Node(l, v, r, _, _) -> bal (usun_min_element l) v r
+        | Empty -> invalid_arg "blad"
+    in
+    match s1, s2 with
+    | Empty, _ -> s2
+    | _, Empty -> s1
+    | _ -> 
+        let r = min_element s2 in
+        let s3 = usun_min_element s2 in 
+        join s1 r s3
 
 (* Funkcje z interfejsu *)
 
 (* Funkcja zwraca pusty zbior *)
-let empty = Empty 
+let empty = Empty
 
 (* Funkcja sprawdza czy zbior jest pusty, zwraca 'true' gdy tak, 'false' gdy nie *)
-let is_empty s = 
-    match s with
+let is_empty = 
+    function
     | Empty -> true
     | Node(_) -> false
 
@@ -245,8 +237,8 @@ let is_empty s =
 let mem x s =
     let rec walk =
         function
-        | Node({left = l; value = (a1, a2); right = r;}) ->
-                (a1 <= x && x <= a2) || walk (if x < a1 then l else r)
+        | Node(l, (v1, v2), r, _, _) ->
+            (v1 <= x && x <= v2) || walk (if x < v1 then l else r)
         | Empty -> false
     in 
     walk s
@@ -256,8 +248,8 @@ let iter f s =
     let rec walk =
         function
         | Empty -> ()
-        | Node({left = l; value = v; right = r;}) ->
-                walk l; f v; walk r
+        | Node(l, v, r, _, _) ->
+            walk l; f v; walk r
     in 
     walk s
 
@@ -269,8 +261,8 @@ let fold f s a =
     let rec walk a =
         function
         | Empty -> a
-        | Node({left = l; value = v; right = r;}) ->
-                walk (f v (walk a l)) r
+        | Node(l, k, r, _, _) ->
+            walk (f k (walk a l)) r
     in 
     walk a s
 
@@ -279,31 +271,10 @@ let elements s =
     let rec walk a =
         function
         | Empty -> a
-        | Node({left = l; value = v; right = r;}) ->
-                walk (v :: walk a r) l
+        | Node(l, v, r, _, _) ->
+            walk (v :: walk a r) l
     in 
     walk [] s
-
-(* 
-    Zwraca liczbę elementow w drzewie mniejszych/rownych x.
-    Przechodząc po drzewie kumulujemy wynik, jeśli dojdziemy do przedzialu
-    zawierającego x, to zwracamy wynik. W przeciwnym razie
-    dodajemy liczebnosc nastepnych elementow mniejszych/rownych x 
-*)
-let below x s =
-    let rec walk s =
-        match s with
-        | Node({left = l; value = (v1, v2); right = r;}) ->
-                let c = cmp (x, x) (v1, v2) in
-                if c = 0 then
-                    if x - v1 + 1 <= 0 then max_int
-                    else suma (elementy_poddrzewa l) (rozmiar_przedzialu l) (x - v1 + 1) 0
-                else
-                    if c < 0 then walk l
-                    else suma (elementy_poddrzewa l) (rozmiar_przedzialu l) (rozmiar_przedzialu s) (walk r)
-        | Empty -> 0
-    in 
-    walk s
 
 (*
     Zwraca drzewo zawierające wszystkie przedziały z 's' oraz przedzial 'x'.
@@ -314,13 +285,36 @@ let below x s =
     z przedziału 'x2' i przedzialow mniejszych i wiekszych
 *)
 let add x s =
+    if fst x > snd x then raise Bledny_Przedzial 
+    else let (lewy_skrajny, prawy_skrajny) = znajdz_skrajne s x in
+        if lewy_skrajny = x && prawy_skrajny = x then add_one x s else
+        let (l1, _, r1) = rozdziel lewy_skrajny s in
+        let (_, _, r2) = rozdziel prawy_skrajny s in
+        let x2 = (min (fst lewy_skrajny) (fst x), max (snd prawy_skrajny) (snd x)) in
+        join l1 x2 r2
+
+(* 
+    Usuwa z drzewa przedzial 'x' i zwraca nowo powstałe
+    drzewo. Szukamy skrajnie lewego i prawego przedziału w dla
+    'x', a potem znajdujemy przedziały
+    mniejsze i większe od y, gdzie y jest przedziałem utworzonym z 'x'
+    i skrajnie lewego i prawego przedziału. Następnie laczymy je dodajac
+    do nich skrajnie lewy i prawy przedział bez czesci wspplnej z 'x' 
+*)
+let remove x s =
     if fst x > snd x then raise Bledny_Przedzial else
-        let (l', r') = znajdz_skrajne s x in
-        if l' = x && r' = x then add_one x s else
-        let (l_tree, _, r1_tree) = rozdziel l' s in
-        let (_, _, r_tree) = rozdziel r' s in
-        let x2 = (min (fst l') (fst x), max (snd r') (snd x)) in
-        join l_tree x2 r_tree
+    let (lewe_skrajne, prawe_skrajne) = znajdz_skrajne s x in
+    let (l1, _, r1) = rozdziel lewe_skrajne s in
+    let (_, _, r2) = rozdziel prawe_skrajne r1 in
+    let nowe_s = sklej l1 r2 in
+    let nowe_s2 =
+        if fst lewe_skrajne < fst x
+        then add (fst lewe_skrajne, fst x - 1) nowe_s
+        else nowe_s
+    in
+    if snd prawe_skrajne > snd x
+    then add (snd x + 1, snd prawe_skrajne) nowe_s2
+    else nowe_s2
 
 (* 
     Zwraca krotke ('l', 'present', 'r'), gdzie 'l' to
@@ -348,27 +342,25 @@ let split x s =
     (l2, mem x s, r3)
 
 (* 
-    Usuwa z drzewa przedzial 'x' i zwraca nowo powstałe
-    drzewo. Szukamy skrajnie lewego i prawego przedziału w dla
-    'x', a potem znajdujemy przedziały
-    mniejsze i większe od y, gdzie y jest przedziałem utworzonym z 'x'
-    i skrajnie lewego i prawego przedziału. Następnie laczymy je dodajac
-    do nich skrajnie lewy i prawy przedział bez czesci wspplnej z 'x' 
+    Zwraca liczbę elementow w drzewie mniejszych/rownych x.
+    Przechodząc po drzewie kumulujemy wynik, jeśli dojdziemy do przedzialu
+    zawierającego x, to zwracamy wynik. W przeciwnym razie
+    dodajemy liczebnosc nastepnych elementow mniejszych/rownych x 
 *)
-let remove x s =
-  if fst x > snd x then raise Bledny_Przedzial else
-    let (lewy_skraj, prawy_skraj) = znajdz_skrajne s x in
-    let (l1, _, r1) = rozdziel lewy_skraj s in
-    let (_, _, r2) = rozdziel prawy_skraj r1 in
-    let nowe_s = sklej l1 r2 in
-    let nowe_s_2 =
-        if fst lewy_skraj < fst x
-        then add (fst lewy_skraj, fst x - 1) nowe_s
-        else nowe_s
-    in
-    if snd prawy_skraj > snd x
-    then add (snd x + 1, snd prawy_skraj) nowe_s_2
-    else nowe_s_2
+let below x s =
+    let rec walk s =
+    match s with
+    | Node(l, (v1, v2), r, _, _) ->
+        let c = cmp (x, x) (v1, v2) in
+        if c = 0 then
+            if x - v1 + 1 <= 0 then max_int
+            else suma (elementy_poddrzew l) (rozmiar_przedzialu l) (x - v1 + 1) 0
+        else
+            if c < 0 then walk l
+            else suma (elementy_poddrzew l) (rozmiar_przedzialu l) (rozmiar_przedzialu s) (walk r)
+    | Empty -> 0
+  in 
+  walk s
 
 (* Testy *)
 (*
